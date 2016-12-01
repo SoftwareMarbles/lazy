ec2bf582b2a5eab7cfe0f5a3a0882bf4db1fe11a
ierceg <ivan@softwaremarbles.com>
2016-12-01 15:37:39 -0300

    Adapt logger to output through lazy logger

--- app/logger.js
+++ app/logger.js
@@ -2,27 +2,25 @@
 'use strict';
 
 const winston = require('winston');
-const LogstashUDP = require('winston-logstash-udp').LogstashUDP;
 
-const LAZY_STACK_VERSION = require('../package.json').version;
+const STACK_VERSION = require('../package.json').version;
+
+//  TODO: Detect when stack container is being run by lazy or on its own and based
+//      on that change the output (e.g. timestamp needed running on its own)
+//  TODO: Detect when stack container is being run by lazy and instead of logging to console
+//      send the output to lazy. The same mechanism should be used with engines while other
+//      containers created and run by the engines should have their standard out/err output
+//      captured (like we do now for lazy-engines-stack)
 
 const logger = new(winston.Logger)({
     transports: [
         new (winston.transports.Console)({
-            timestamp: () => {
-                return new Date();
-            },
             formatter: (options) => {
-                return options.timestamp().toISOString() + ' [' + LAZY_STACK_VERSION + '] ' +
+                return '[' + STACK_VERSION + '] ' +
                     options.level.toUpperCase() + ' ' + (options.message ? options.message : '') +
                     (options.meta && Object.keys(options.meta).length ?
                         '\n\t'+ JSON.stringify(options.meta) : '');
             }
-        }),
-        new(LogstashUDP)({
-            appName: 'lazy-stack',
-            host: process.env.ELK_HOST,
-            port: process.env.ELK_PORT
         })
     ]
 });
