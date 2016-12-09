b5894b4bc7b1d256b7db67485c85831977669ca0
ierceg <ivan@softwaremarbles.com>
2016-12-09 02:54:33 -0300

    Remove obsolete comments

--- app/logger.js
+++ app/logger.js
@@ -5,13 +5,6 @@ const winston = require('winston');
 
 const STACK_VERSION = require('../package.json').version;
 
-//  TODO: Detect when stack container is being run by lazy or on its own and based
-//      on that change the output (e.g. timestamp needed running on its own)
-//  TODO: Detect when stack container is being run by lazy and instead of logging to console
-//      send the output to lazy. The same mechanism should be used with engines while other
-//      containers created and run by the engines should have their standard out/err output
-//      captured (like we do now for lazy-engines-stack)
-
 const logger = new(winston.Logger)({
     transports: [
         new (winston.transports.Console)({
