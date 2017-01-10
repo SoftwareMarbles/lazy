93894db9a0e1c0b5e0342c9af231b225981e409c
ierceg <ivan@softwaremarbles.com>
2017-01-10 19:53:38 -0300

    Fix TODO link

--- app/controllers/internal/config-controller.js
+++ app/controllers/internal/config-controller.js
@@ -6,7 +6,7 @@
 const _ = require('lodash');
 
 const initialize = (app, options) => {
-    //  TODO: See https://github.com/SoftwareMarbles/lazy/issues/44 for proposal to change
+    //  TODO: See https://github.com/getlazy/lazy/issues/44 for proposal to change
     //  this endpoint.
     app.get('/config', (req, res) => {
         const engineName = _.toLower(_.get(req, 'query.engine'));
