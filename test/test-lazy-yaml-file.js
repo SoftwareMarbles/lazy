d96fded7b998c516439fb27346b001ee7a533c8c
ierceg <ivan@softwaremarbles.com>
2017-01-13 00:03:40 -0300

    Fix more lazy warnings

--- test/test-lazy-yaml-file.js
+++ test/test-lazy-yaml-file.js
@@ -4,11 +4,12 @@
 /* global logger, describe, it, before, after, afterEach */
 
 //  To set some properties we need `this` of `describe` and `it` callback functions.
-/* eslint prefer-arrow-callback: off, func-names: off, class-methods-use-this: off, lodash/prefer-constant: off */
+// lazy ignore prefer-arrow-callback
+// lazy ignore func-names
+// lazy ignore class-methods-use-this
+// lazy ignore lodash/preferred-alias
 
 require('./bootstrap');
-
-const td = require('testdouble');
 
 const _ = require('lodash');
 const assert = require('assert');
@@ -17,7 +18,7 @@ const configTests = require('./fixtures/lazy-yaml-file-config-tests');
 
 describe('LazyYamlFile', function () {
     describe('_getConfigErrors', function () {
-        _.each(configTests, test => {
+        _.each(configTests, (test) => {
             it(`schema check test ${test.id}`, function () {
                 const errors = LazyYamlFile._getConfigErrors(test.config);
                 if (test.firstErrorMessage) {
