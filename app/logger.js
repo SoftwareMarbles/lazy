56f08b9fa038e5ef075eb53b351afaf41131cdbc
ierceg <ivan@softwaremarbles.com>
2017-01-18 23:36:29 -0300

    Add "metric" logging level
    Add colors to console output
    Standardize console output

--- app/logger.js
+++ app/logger.js
@@ -8,6 +8,31 @@ const fp = require('lodash/fp');
 
 const LAZY_VERSION = require('../package.json').version;
 
+// Based on npm logging levels but with more space to add additional future logging levels
+// like "metric".
+const LazyLoggingLevels = {
+    levels: {
+        error: 0,
+        warn: 10,
+        info: 20,
+        // `metric` is a special logging level which we use to log all our explicit metrics.
+        metric: 25,
+        verbose: 30,
+        debug: 40,
+        silly: 50
+    },
+    colors:
+    {
+        error: 'red',
+        warn: 'yellow',
+        info: 'green',
+        metric: 'grey',
+        verbose: 'cyan',
+        debug: 'blue',
+        silly: 'magenta'
+    }
+};
+
 const initialize = (lazyConfig) => {
     const transports = [];
 
@@ -24,28 +49,29 @@ const initialize = (lazyConfig) => {
     let consoleConfig = _.get(lazyConfig, 'config.logger.console');
     if (!consoleConfig && _.isEmpty(transports)) {
         consoleConfig = {
-            level: 'info'
+            level: 'metric'
         };
     }
 
     if (consoleConfig) {
         transports.push(new winston.transports.Console({
-            timestamp: () => new Date(),
+            timestamp: () => new Date().toISOString(),
             level: consoleConfig.level,
-            formatter: (options) => {
-                const message = options.message ? options.message : '';
-                const meta = (options.meta && !_.isEmpty(options.meta)) ?
-                    JSON.stringify(options.meta) : '';
-                return `${options.timestamp().toISOString()} ${options.level.toUpperCase()} ${message} ${meta}`;
-            }
+            colorize: true
         }));
     }
+
+    winston.addColors(LazyLoggingLevels.colors);
 
     const logger = new winston.Logger({
         transports,
         rewriters: [
             (level, message, meta) => fp.extend(meta, { lazy_version: LAZY_VERSION })
-        ]
+        ],
+        levels: LazyLoggingLevels.levels
+    });
+    logger.on('error', (err) => {
+        console.log('Logging error', err);
     });
 
     return Promise.resolve(logger);
