7f2febf43eca0703a907cd23b435ad146ad9e93e
ierceg <ivan@softwaremarbles.com>
2017-01-28 01:57:47 -0300

    Add empty tests to correct coverage reports

---
+++ test/test-engine.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* global logger, describe, it, before, after, afterEach */
+
+//  To set some properties we need `this` of `describe` and `it` callback functions.
+// lazy ignore prefer-arrow-callback
+// lazy ignore func-names
+
+const td = require('testdouble');
+
+const _ = require('lodash');
+const assert = require('assert');
+const Engine = require('../app/engine');
+
+describe('Engine', function () {
+});
